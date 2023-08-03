fd90e912c83d0ba180e8003508b86926fcfe9238
Home <Home@arushi>
2023-08-03 19:54:49 +0530

    demo is committed

---
+++ src/test/java/sampleGit/SampleProgram.java
@@ -0,0 +1,16 @@
+package sampleGit;
+
+import org.testng.annotations.Test;
+
+public class SampleProgram {
+	@Test
+	
+		public void demo()
+		{
+		System.out.println("hi this is my first commit");
+		}
+		
+		
+	
+
+}
